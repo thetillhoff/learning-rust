68793c11ed76b6398698c2e5264a4935190123f3
Till Hoffmann <till@thetillhoff.de>
2025-10-17 14:01:59 +0200

    Add line-by-line writer

--- dfc/src/main.rs
+++ dfc/src/main.rs
@@ -1,17 +1,37 @@
 use clap::Parser;
-use std::io::BufRead;
+use std::io::{BufRead, Write};
 
 mod cli;
 mod reader;
+mod writer;
 
 fn main() -> anyhow::Result<()> {
     let args = cli::Args::parse();
     let source_format = cli::source_format(args.source.as_ref(), args.source_format.as_ref());
     let destination_format = cli::destination_format(args.destination.as_ref(), args.destination_format.as_ref());
     let reader = reader::open_reader(args.source.as_ref())?;
-    for line in reader.lines() {
-        println!("{}", line.unwrap())
+
+    let mut writer = writer::open_writer(args.destination.as_ref())?;
+
+    match source_format.as_str() {
+        "csv" => {
+            println!("csv parser");
+            for line in reader.lines() {
+                let line_content = line?;
+                writer.write_all(line_content.as_bytes())?;
+                writer.write_all(b"\n")?;
+            };
+        }
+        "json" => {
+            println!("json parser");
+        }
+        _ => {
+            println!("unknown format");
+        }
     }
+
+    writer.flush()?;
+
     println!("source_format: {:?}", source_format);
     println!("destination_format: {:?}", destination_format);
     Ok(())
---
+++ dfc/src/writer.rs
@@ -0,0 +1,17 @@
+use anyhow::{Result, bail};
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use std::path::Path;
+
+pub(crate) fn open_writer(destination: Option<&String>) -> Result<BufWriter<File>> {
+    match destination {
+        None => {
+            bail!("Could not open destination file")
+        }
+        Some(destination) => {
+            let path = Path::new(destination);
+            let file = File::create(&path)?;
+            Ok(BufWriter::new(file))
+        }
+    }
+}
