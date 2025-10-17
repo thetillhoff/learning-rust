fba2c997552660bf36bbbd47c0beb57e85930f08
Till Hoffmann <till@thetillhoff.de>
2025-10-17 14:54:30 +0200

    Implement CSV reading and writing functionality, including delimiters and header handling

--- dfc/src/main.rs
+++ dfc/src/main.rs
@@ -1,5 +1,4 @@
 use clap::Parser;
-use std::io::{BufRead, Write};
 
 mod cli;
 mod reader;
@@ -9,18 +8,36 @@ fn main() -> anyhow::Result<()> {
     let args = cli::Args::parse();
     let source_format = cli::source_format(args.source.as_ref(), args.source_format.as_ref())?;
     let destination_format = cli::destination_format(args.destination.as_ref(), args.destination_format.as_ref())?;
-    let reader = reader::open_reader(args.source.as_ref())?;
 
+    println!("source_format: {:?}", source_format);
+    println!("destination_format: {:?}", destination_format);
+
+    let reader = reader::open_reader(args.source.as_ref())?;
     let mut writer = writer::open_writer(args.destination.as_ref())?;
 
     match source_format.as_str() {
         "csv" => {
-            println!("csv parser");
-            for line in reader.lines() {
-                let line_content = line?;
-                writer.write_all(line_content.as_bytes())?;
-                writer.write_all(b"\n")?;
+
+            let mut csv_writer = csv::WriterBuilder::new()
+                .delimiter(b';') // TODO reuse same delimiter and make it configurable
+                .from_writer(&mut writer);
+            // TODO Note that the CSV writer is buffered automatically, so you should not wrap wtr in a buffered writer like io::BufWriter.
+            //      https://docs.rs/csv/latest/csv/struct.Writer.html
+
+            let mut csv_reader = csv::ReaderBuilder::new()
+                .delimiter(b',') // TODO get delimiter from file and make it configurable
+                .from_reader(reader);
+
+            // TODO make this configurable
+            let headers = csv_reader.headers()?;
+            csv_writer.write_record(headers)?;
+
+            for result in csv_reader.records() {
+                let record = result?;
+                csv_writer.write_record(&record)?;
             };
+
+            csv_writer.flush()?;
         }
         "json" => {
             println!("json parser");
@@ -30,10 +47,6 @@ fn main() -> anyhow::Result<()> {
         }
     }
 
-    writer.flush()?;
-
-    println!("source_format: {:?}", source_format);
-    println!("destination_format: {:?}", destination_format);
     Ok(())
     // read file - batch wise / streaming
     // write file - batch wise / streaming
