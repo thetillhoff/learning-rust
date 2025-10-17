5492353286c2bbf02ba3b3f662e5435d387db117
Alexander Bokov <alexander.bokov@bonprix.net>
2025-10-17 16:30:45 +0200

    Source type encapsulating readers like CSV, Fixed-Width etc.

--- dfc/src/reader.rs
+++ dfc/src/reader.rs
@@ -15,3 +15,33 @@ pub(crate) fn open_reader(source: Option<&String>) -> Result<BufReader<File>> {
         }
     }
 }
+
+// Maybe could be helpful later if we want to define some methods like filter, map, dropColumn, etc.
+struct Record {
+    inner: Vec<String>
+}
+
+struct CsvSource {
+    delimeter: String,
+    reader: csv::Reader<std::io::BufReader<String>>,
+}
+
+struct FixedWidthSource {
+    spec: Vec<u16>,
+    reader: std::io::BufReader<String>,
+}
+
+impl Iterator for CsvSource {
+    type Item = Vec<String>;
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!("");
+    }
+}
+
+impl Iterator for FixedWidthSource {
+    type Item = Vec<String>;
+    fn next(&mut self) -> Option<Self::Item> {
+        todo!("");
+    }
+}
+
